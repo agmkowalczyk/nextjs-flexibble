b3b7f58e86de1e9274f51b99243a31bb4706b907
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-12-09 02:40:20 +0100

    ProjectForm

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -5,6 +5,9 @@ import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { FormState, ProjectInterface, SessionInterface } from '@/common.types'
 import Image from 'next/image'
 import FormField from './FormField'
+import { categoryFilters } from '@/constants'
+import CustomMenu from './CustomMenu'
+import Button from './Button'
 
 type Props = {
   type: string
@@ -13,15 +16,46 @@ type Props = {
 }
 
 const ProjectForm = ({ type, session, project }: Props) => {
-  const form = { image: '', title: '' }
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
+  const [form, setForm] = useState({
+    title: project?.title || '',
+    description: project?.description || '',
+    image: project?.image || '',
+    liveSiteUrl: project?.liveSiteUrl || '',
+    githubUrl: project?.githubUrl || '',
+    category: project?.category || '',
+  })
 
   const handleFormSubmit = async (e: FormEvent) => {
     e.preventDefault()
   }
 
-  const handleChangeImage = (e: ChangeEvent) => {}
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
 
-  const handleStateChange = (fieldName: string, value: string) => {}
+    const file = e.target.files?.[0]
+
+    if (!file) return
+
+    if (!file.type.includes('image')) {
+      return alert('Please upload an image!')
+    }
+
+    const reader = new FileReader()
+
+    reader.readAsDataURL(file)
+
+    reader.onload = () => {
+      const result = reader.result as string
+
+      handleStateChange('image', result)
+    }
+  }
+
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }))
+  }
 
   return (
     <form onSubmit={handleFormSubmit} className='flexStart form'>
@@ -53,6 +87,50 @@ const ProjectForm = ({ type, session, project }: Props) => {
         placeholder='Flexibble'
         setState={(value) => handleStateChange('title', value)}
       />
+
+      <FormField
+        title='Description'
+        state={form.description}
+        placeholder='Showcase and discover remarkable developer projects.'
+        isTextArea
+        setState={(value) => handleStateChange('description', value)}
+      />
+
+      <FormField
+        type='url'
+        title='Website URL'
+        state={form.liveSiteUrl}
+        placeholder='https://example.com'
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+
+      <FormField
+        type='url'
+        title='GitHub URL'
+        state={form.githubUrl}
+        placeholder='https://github.com/example'
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+
+      <CustomMenu
+        title='Category'
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+
+      <div className='flexStart w-full'>
+        <Button
+          title={
+            isSubmitting
+              ? `${type === 'create' ? 'Creating' : 'Editing'}`
+              : `${type === 'create' ? 'Create' : 'Edit'}`
+          }
+          type='submit'
+          leftIcon={isSubmitting ? '' : '/plus.svg'}
+          isSubmitting={isSubmitting}
+        />
+      </div>
     </form>
   )
 }
