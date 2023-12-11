5c74abb290be50cc9fb9e8aca89048055bafd7c7
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-12-11 18:34:29 +0100

    ProjectCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,53 @@
-const Home = () => {
+import { ProjectInterface } from '@/common.types'
+import ProjectCard from '@/components/ProjectCard'
+import { fetchAllProjects } from '@/lib/actions'
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[]
+    pageInfo: {
+      hasPreviousPage: boolean
+      hasNextPage: boolean
+      startCursor: string
+      endCursor: string
+    }
+  }
+}
+
+const Home = async () => {
+  const data = (await fetchAllProjects()) as ProjectSearch
+
+  const projects = data?.projectSearch?.edges || []
+
+  if (projects.length === 0) {
+    return (
+      <section className='flexStart flex-col paddings'>
+        Categories
+        <p className='no-result-text text-center'>
+          No projects found, go create some first.
+        </p>
+      </section>
+    )
+  }
+
   return (
-    <div>Home</div>
+    <section className='flexStart flex-col paddings mb-16'>
+      Categories
+      <section className='projects-grid'>
+        {projects.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={`${node?.id}`}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy.name}
+            avatarUrl={node?.createdBy.avatarUrl}
+            userId={node?.createdBy.id}
+          />
+        ))}
+      </section>
+      LoadMore
+    </section>
   )
 }
 
